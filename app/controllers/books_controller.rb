98286a4204c61a593779253404c482d04ddcc029
ogmegumi <ogmegumi@gmail.com>
2021-06-15 16:15:28 +0000

    [Update}4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
-    @book = Book.all
+    @books = Book.all
+    @user = current_user
   end
   
   def create
+    @user = current_user
     @book = Book.new(book_params)#データを新規登録するためのインスタンス作成 
     @book.save
-    redirect_to '/top'
+    redirect_to book_path(@book.id)
+  end
+  
+  def show
+   @user = current_user
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,34 @@
 class UsersController < ApplicationController
+    def index
+      @user = User.new
+      @users = User.all
+    end
     
-  def index
-    @user = User.all
-    @user = User.new
-    @user = current_user
-  end
+    def create
+     @user = User.new(user_image_params)
+     @user.user_id = current_user.id
+     @user.save
+     redirect_to edit_user_path
+    end
   
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-  
-
-
-
+    def show
+      @user = User.find(params[:id])
+    end
+    
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      @user.update(user_params)
+      redirect_to edit_path(@user)
+    end
+    
+    private
+    
+    def user_image_params
+      params.require(:user_image).permit(:name, :profile_image, :introduction)
+    end
+    
 end
